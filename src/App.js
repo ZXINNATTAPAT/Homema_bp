e1d1abd085482461f251553ea14be1ac346fd609
Nattapatscz <s6404062630333@email.kmutnb.ac.th>
2023-08-12 20:59:14 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+          <section classsname ="">
+            <figure class="text-center">
+              <blockquote class="blockquote">
+                <p>A well-known quote, contained in a blockquote element.</p>
+              </blockquote>
+              <figcaption class="blockquote-footer">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </figcaption>
+            </figure>
+          </section>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
